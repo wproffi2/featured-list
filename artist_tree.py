9ed94219cf101609ad26e65235a864441cc65129
warrie <warrie.proffitt@basecampcs.com>
2019-07-16 20:52:51 -0400

    created ArtistTree class

--- artist_tree.py
+++ artist_tree.py
@@ -3,19 +3,43 @@ from feat_artists import FeaturedArtists
 import matplotlib.pyplot as plt
 
 
-parent_artist = 'Tyler, The Creator'
-search = FeaturedArtists(parent_artist)
-data = search.collectData()
-G = nx.Graph()
-G.add_node(parent_artist)
-for x in data:
-    song, artists = x[0], list(x[1])
-    G.add_node(song)
-    G.add_edge(parent_artist, song)
+class ArtistTree:
+    def __init__(self):
+        self.tree = nx.Graph()
+        self.color_map = []
+    
+    def display(self):
+        #nx.draw(self.tree, node_color = self.color_map, with_labels=True)
+        nx.draw(self.tree, with_labels=True)
+        plt.show()
 
-    for artist in artists:
-        G.add_node(artist)
-        G.add_edge(song, artist)
+    def updateTree(self, parent_artist, data):
+        self.tree.add_node(parent_artist)
+        #self.color_map.append('green')
 
-nx.draw(G)
-plt.show()
+        for x in data:
+            song, artists = x[0], list(x[1])
+            self.tree.add_node(song)
+            #self.color_map.append('blue')
+
+            self.tree.add_edge(parent_artist, song)
+
+            for artist in artists:
+                self.tree.add_node(artist)
+                #self.color_map.append('green')
+                self.tree.add_edge(song, artist)
+
+        
+        return 0
+
+
+artists = ['Tyler, The Creator', "Rex Orange County", "Jaden", "Kid Cudi"] #'A$AP Rocky'
+test = ArtistTree()
+for artist in artists:
+    parent_artist = artist
+    search = FeaturedArtists(parent_artist)
+    data = search.collectData()
+    test.updateTree(parent_artist, data)
+
+test.display()
+
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-#testing args 
+import networkx as nx
+from feat_artists import FeaturedArtists
+import matplotlib.pyplot as plt
 
-class ArgTest:
-    def __init__(self, *args):
-        self.args = list(args)
-    
-    def test(self):
-        print(self.args)
-
-args = ["Sammy", "Casey", "Alex"]
-x = ArgTest(*args)
-x.test()
+G = nx.erdos_renyi_graph(20,0.1)
+color_map = []
+for node in G:
+    if node <10:
+        color_map.append('blue')
+    else: color_map.append('green')      
+nx.draw(G,node_color = color_map,with_labels = True)
+plt.show()
