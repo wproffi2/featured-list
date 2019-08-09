b33e91898f46ce8d40d72960844e90d1e75d0f80
Warrie Proffitt <Warrie.Proffitt@ablevets.com>
2019-08-09 10:09:17 -0400

    updated host

--- web/featured_list/settings.py
+++ web/featured_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e(z2=z!n=-fol6x3(b3ffzd#b&j$7sdiv3v*7@g!60d-yh&a#u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-52-87-188-222.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['ec2-54-80-18-14.compute-1.amazonaws.com']
 
 
 # Application definition
